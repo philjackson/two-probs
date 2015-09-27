ad19663739b50961aeb5eebf301d6c5bdc1b70a5
Phil Jackson <phil@shellarchive.co.uk>
2015-09-27 20:32:37 +0100

    Add support for or'ing.

--- src/re/core.clj
+++ src/re/core.clj
@@ -1,23 +1,40 @@
-(ns re.core)
+(ns re.core
+  (:require [clojure.string :as str]))
 
 (declare re)
 
-(defn group [expressions]
-  (str "(" (apply re expressions) ")"))
+(defn group
+  ([expressions] (group expressions true))
+  ([expressions capturing?] (str "("
+                                 (when-not capturing? "?:")
+                                 (re expressions)
+                                 ")")))
 
-(defn re [& expressions]
+(defn zero-or-one [expressions]
+  (str (if (> (count expressions) 1)
+         (group expressions false)
+         (re expressions))
+       "?"))
+
+(defn one-or-more [expressions]
+  (str (if (> (count expressions) 1)
+         (group expressions false)
+         (re expressions))
+       "+"))
+
+(defn re [expressions]
   (loop [expressions expressions
          current ""]
-    (let [command (first expressions)]
-      (let [gen (str current (cond
-                               (vector? command) (case (first command)
-                                                   :group (group (rest command)))
-                               (= command :digit) "\\d"
-                               (= command :non-digit) "\\D"
-                               (= command :word) "\\w"
-                               (= command :non-word) "\\W"
-                               (= command :space) "\\s"
-                               (= command :non-space) "\\S"))]
-        (if (> (count (rest expressions)) 0)
-          (recur (rest expressions) gen)
-          gen)))))
+    (let [command (first expressions)
+          gen (str current (cond
+                             (string? command) command
+                             (vector? command) (group [(str/join "|" (map #(re [%]) command))] false)
+                             (= command :digit) "\\d"
+                             (= command :non-digit) "\\D"
+                             (= command :word) "\\w"
+                             (= command :non-word) "\\W"
+                             (= command :space) "\\s"
+                             (= command :non-space) "\\S"))]
+      (if (> (count (rest expressions)) 0)
+        (recur (rest expressions) gen)
+        gen))))
--- test/re/core_test.clj
+++ test/re/core_test.clj
@@ -2,22 +2,27 @@
   (:require [clojure.test :refer :all]
             [re.core :refer :all]))
 
-[:group
- [:count [:digit] 12]
- [:word]]
-
 (deftest re-test
   (testing "basics"
-    (are [s r] (= s (apply re r))
+    (are [s r] (= s (apply re [r]))
       "\\d" [:digit]
       "\\D" [:non-digit]
       "\\w" [:word]
       "\\W" [:non-word]
       "\\s" [:space]
       "\\S" [:non-space]
-
       "\\S\\w" [:non-space :word]))
 
-  (testing "grouping"
-    (are [s r] (= s (re r))
-      "(\\w\\s)" [:group :word :space])))
+  (testing "string handling"
+    (are [s r] (= s (re [r]))
+      "(\\w\\s)" (group [:word :space])))
+
+  (testing "or-ing"
+    (is (= "(?:one|two)" (re [["one" "two"]]))))
+
+  (testing "groupings"
+    (is (= "(\\w\\s)"    (group [:word :space])))
+    (is (= "\\w+"        (one-or-more [:word])))
+    (is (= "\\w?"        (zero-or-one [:word])))
+    (is (= "(?:\\w\\s)?" (zero-or-one [:word :space])))
+    (is (= "(?:\\w\\s)+" (one-or-more [:word :space])))))
