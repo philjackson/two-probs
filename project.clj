644ac8871ae57cbdbbf365aecac05bc12bfbe32b
Phil Jackson <phil@shellarchive.co.uk>
2015-09-29 15:22:09 +0100

    Update project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject two-probs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject two-probs "0.1.0"
+  :description "If you're unlucky enough to have to process large regexps, this library should help you make them more readable, composable and maintainable."
+  :url "https://github.com/philjackson/two-probs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
