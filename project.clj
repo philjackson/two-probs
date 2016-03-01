f745ac681d176311a99fbdccf3e150a3c4e4ef13
Phil Jackson <phil@shellarchive.co.uk>
2016-03-01 14:34:08 +0000

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject philjackson/two-probs "0.1.2"
+(defproject philjackson/two-probs "0.1.3"
   :description "If you're unlucky enough to have to process large regexps, this library should help you make them more readable, composable and maintainable."
   :url "https://github.com/philjackson/two-probs"
   :license {:name "Eclipse Public License"
